a1a3914b52dba3062124f3aad0ea762872473018
Madhuranga Senadheera <lilan.maduranga@gmail.com>
2023-08-13 16:59:29 +0400

    Added paginations

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React from "react";
+import Link from "next/link";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+interface PaginationProps {
+    currentPage: number;
+    totalPages: number;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages }) => {
+    const handlePageChange = (newPage: number) => {
+        if (newPage >= 1 && newPage <= totalPages) {
+            window.location.href = `/users?page=${newPage}`;
+        }
+    };
+
+    const paginationLinks = [];
+    for (let i = 1; i <= totalPages; i++) {
+        const currentLink = <Link href={`/users?page=${i}`} key={i}> {' '}{i}{' '} </Link>;
+        paginationLinks.push((currentPage == i ? i : currentLink));
+    }
+
+    return (
+        <div>
+            {
+                currentPage != 1 && <FaChevronLeft
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    className="pagination-arrow"
+                />
+            }
+            {paginationLinks}
+            {
+                currentPage < totalPages &&<FaChevronRight
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    className="pagination-arrow"
+                />
+            }
+        </div>
+    );
+};
+
+export default Pagination;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,41 +1,57 @@
+'use client'
+
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import Pagination from "../components/Pagination";
 
 async function getUsers(page: number = 1) {
-  const res = await fetch(
-    `${process.env.BASE_URL}/api/users/?page=${page}`,
-    { cache: 'no-store' }
-  )
+  const res = await fetch(`/api/users/?page=${page}`, { cache: 'no-store' });
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
   return res.json();
 }
 
-
-function onDeleteHandler(id: number) {
-  console.log(`Delete user `, id);
-}
-
-interface UserData {
-  id: number;
-  name: string;
-  email: string;
-  role: string;
-}
-
-export default async function Page({ searchParams: { page } }: { searchParams: { page: number } }) {
-  const { users, pagination: { currentPage, totalPages, totalUsers } } = await getUsers(page);
-
-  const paginationLinks = [];
-  for (let i = 1; i <= totalPages; i++) {
-    paginationLinks.push(
-      <Link href={`/users?page=${i}`} key={i}>
-        {' '}{i}{' '}
-      </Link>
-    );
+async function deleteUser(id: number) {
+  const res = await fetch(`/api/users/${id}`, {
+    method: "DELETE",
+  });
+  if (!res.ok) {
+    throw new Error('Failed to delete data');
   }
+  return res.json();
+}
 
-  const usersData = users.map(({ id, name, email, role }: UserData) => {
+export default function Page({ searchParams: { page } }: { searchParams: { page: number } }) {
+  const [usersData, setUsersData] = useState<UserData[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  // const [totalUsers, setTotalUsers] = useState<number>(0);
+  
+  useEffect(() => {
+    if(page>0) {
+      setCurrentPage(page);
+    }
+
+    async function fetchData() {
+      const { users, pagination: { currentPage, totalPages, totalUsers } } = await getUsers(page);
+      setUsersData(users);
+      // setCurrentPage(currentPage);
+      setTotalPages(totalPages);
+      // setTotalUsers(totalUsers);
+    }
+    fetchData();
+  }, [page]);
+
+  const onDeleteHandler = async (id: number) => {
+    if (confirm("Are you sure to delete the user ?")) {
+      await deleteUser(id);
+      const { users } = await getUsers(page);
+      setUsersData(users);
+    }
+  };
+
+  const renderedUsers = usersData.map(({ id, name, email, role }: UserData) => {
     return (
       <tr key={id}>
         <td>{name}</td>
@@ -44,11 +60,12 @@ export default async function Page({ searchParams: { page } }: { searchParams: {
         <td>
           <Link href={`/users/view/${id}`}>View </Link> |{' '}
           <Link href={`/users/edit/${id}`}> Update </Link> |{' '}
-          <Link href={`/users/delete/${id}`}> Delete</Link>
+          <button onClick={() => onDeleteHandler(id)}>Delete</button>
         </td>
       </tr>
     );
   });
+
   return (
     <>
       <h1>User List</h1>
@@ -63,12 +80,10 @@ export default async function Page({ searchParams: { page } }: { searchParams: {
           </tr>
         </thead>
         <tbody>
-          {usersData}
+          {renderedUsers}
         </tbody>
       </table>
-      <div>
-        {paginationLinks}
-      </div>
+      <Pagination currentPage={currentPage} totalPages={totalPages} />
     </>
   );
 }
